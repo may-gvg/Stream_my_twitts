28b3a981a2321c898acb951035892da3f108bb6a
may <83417030+may-gvg@users.noreply.github.com>
2021-11-23 15:49:28 +0100

    Add files via upload

---
+++ spark.py
@@ -0,0 +1,61 @@
+import findspark
+findspark.init("C:\spark")
+from logging import DEBUG
+
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+from pyspark.sql import functions as F
+from textblob import TextBlob
+
+
+def preprocessing(lines):
+    words = lines.select(explode(split(lines.value, "t_end")).alias("word"))
+    words = words.na.replace('', None)
+    words = words.na.drop()
+    words = words.withColumn('word', F.regexp_replace('word', r'http\S+', ''))
+    words = words.withColumn('word', F.regexp_replace('word', '@\w+', ''))
+    words = words.withColumn('word', F.regexp_replace('word', '#', ''))
+    words = words.withColumn('word', F.regexp_replace('word', 'RT', ''))
+    words = words.withColumn('word', F.regexp_replace('word', ':', ''))
+    return words
+
+
+# text classification
+def polarity_detection(text):
+    return TextBlob(text).sentiment.polarity
+
+
+def subjectivity_detection(text):
+    return TextBlob(text).sentiment.subjectivity
+
+
+def text_classification(words):
+    # polarity detection
+    polarity_detection_udf = udf(polarity_detection, StringType())
+    words = words.withColumn("polarity", polarity_detection_udf("word"))
+    # subjectivity detection
+    subjectivity_detection_udf = udf(subjectivity_detection, StringType())
+    words = words.withColumn("subjectivity", subjectivity_detection_udf("word"))
+    return words
+
+
+if __name__ == "__main__":
+    # create Spark session
+    spark = SparkSession.builder.appName("TwitterSentimentAnalysis").getOrCreate()
+    print("HUJJ")
+    sc = spark.sparkContext
+    sc.setLogLevel("DEBUG")
+    # read the tweet data from socket
+    lines = spark.readStream.format("socket").option("host", "127.0.0.1").option("port", 6677).load()
+    # Preprocess the data
+    words = preprocessing(lines)
+    # text classification to define polarity and subjectivity
+    words = text_classification(words)
+    words = words.repartition(1)
+    query = words.writeStream.queryName("all_tweets") \
+        .outputMode("append").format("parquet") \
+        .option("path", "./parc") \
+        .option("checkpointLocation", "./check") \
+        .trigger(processingTime='60 seconds').start()
+    query.awaitTermination()
